ab5eddbdae2fb65c585d683c9007f949bfd74941
PricelessEmpireSy <misspriceless45@gmail.com>
2022-12-11 11:46:43 -0800

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,11 +11,12 @@ int main(void)
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-	if (i != 'e' && ch != 'q')
-	{
-		putchar(i);
-	}
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
-	return (0);
+
+	return (o);
 }
