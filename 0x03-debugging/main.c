361d37f1086756b4c070673cff077a47c19683ed
PricelessEmpireSy <misspriceless45@gmail.com>
2022-12-16 09:02:10 -0800

    Mulitiple mains

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test that the function prints if integer is positive or negative
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
