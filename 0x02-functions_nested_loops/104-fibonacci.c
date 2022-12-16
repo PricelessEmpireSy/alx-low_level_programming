2361421e61585841c4c7d74d342f924e8c4dabec
PricelessEmpireSy <misspriceless45@gmail.com>
2022-12-16 05:55:18 -0800

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,47 +1,63 @@
 #include <stdio.h>
 
+
 /**
- * main - fibonacci <3
- *
- * Purpose - no hardcode
- *
- * Return: (Success)
+ * numLength - returns the length of string
+ * @num: operand number
+ * Return: number of digits
+ */
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+	{
+		return (1);
+	}
+
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+	return (length);
+}
+/**
+ * main - prints the first 100 fibonacci sequences
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
+	short int i = 1, initials;
 
-	printf("%lu", bef);
-
-	for (i = 1; i < 91; i++)
+	while (i <= 98)
 	{
-		printf(",%lu", aft);
-		aft += bef;
-		bef = aft - bef;
-	}
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initials = numLength(mx) - 1 - numLength(f1);
+		while (f1o > 0 && initials > 0)
+		{
+			printf("%i", 0);
+			initials--;
+		}
+		printf("%lu", f1);
 
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
+		tmp = (f1 + f2) % mx;
+		tmpo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = tmp;
+		f2o = tmpo;
 
-	for (i = 92; i < 99; ++i)
-	{
-		printf(",%lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
+		if (i != 100)
+			printf(", ");
+		else
+			printf("\n");
+		i++;
 	}
-	printf("\n");
 	return (0);
 }
