2648ec575ba740c1c60117e5916b92bb2ae8ec1f
PricelessEmpireSy <misspriceless45@gmail.com>
2022-12-20 01:35:41 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,35 +1,18 @@
 #include "main.h"
-
 #include <stdio.h>
 
-
-
 /**
+ * main - check the code.
  *
- *  * main - check the code.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+	char c;
 
-	    char c;
-
-
-
-	        c = 'A';
-
-		    printf("%c: %d\n", c, _isupper(c));
-
-		        c = 'a';
-
-			    printf("%c: %d\n", c, _isupper(c));
-
-			        return (0);
-
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -5,10 +5,9 @@
  * @c: The character to print
  *
  * Return: On Success 1
- * On error, -1 is returned, and error is set appropriately
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-
 	return (write(1, &c, 1));
 }
