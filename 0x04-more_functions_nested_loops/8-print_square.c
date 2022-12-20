f9d4ce8d0e3ee97e23fa1c57fc1af4de067fb8f6
PricelessEmpireSy <misspriceless45@gmail.com>
2022-12-20 11:29:01 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,23 +5,25 @@
  * @size: The number of squares/number of times
  * Return: Empty
  */
+
 void print_square(int size)
+
 {
 	int x, y;
 
 	if (size <= 0)
 	{
-		_putchar('\n')
+	_putchar('\n')
 	}
 	else
 	{
-		for (x = 0; x < size; x++)
-		{
-			for (y = 0; y < size; y++)
-			{
-				_putchar(35);
-			}
-			_putchar('\n');
-		}
+	for (x = 0; x < size; x++)
+	{
+	for (y = 0; y < size; y++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
 	}
 }
