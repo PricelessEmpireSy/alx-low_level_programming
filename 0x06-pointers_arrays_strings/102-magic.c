30aed06f9c9e4ed439a86bf5533a52ca605f3c09
PricelessEmpireSy <misspriceless45@gmail.com>
2022-12-22 14:59:02 -0800

    A dream doesn't become reality through magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,47 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * infinite_add - adds two numbers
- * @n1: First number
- * @n2: Second number
- * @r: Buffer for results
- * @size_r: Buffer size
- * ops! Dreams not magic! Determination and hardwork
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ * Description: prints a[2] = 98
  * Return: Address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+	int n;
+	int a[5];
+	int *p;
 
-	for (i = 0; n1[i]; i++)
-		;
-	for (j = 0; n2[j]; j++)
-		;
-	if (i > size_r || j > size_r)
-		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
-	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
-	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
-		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
-	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
-	}
-	return (r);
+	a[2] = 98;
+	p = &n;
+	(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n" a[2]);
+	return (0);
 }
