fb92bc3d0d431510e0b69a09cdc12587b6a29986
PricelessEmpireSy <misspriceless45@gmail.com>
2022-12-22 15:07:50 -0800

    A dream doesn't become reality through magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -18,7 +18,15 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	a[2] = 98;
 	p = &n;
-	(p + 5) = 98;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n" a[2]);
 	return (0);
