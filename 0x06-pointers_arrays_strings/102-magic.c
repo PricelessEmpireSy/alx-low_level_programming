6e1471828a6bb7597dae450ad67df2edbc4758b0
PricelessEmpireSy <misspriceless45@gmail.com>
2022-12-22 13:22:20 -0800

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,21 +1,47 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - Entry point
- * Description: program prints a[2] = 98, followed by a new line.
- * Return: Always 0 (Success)
+ * infinite_add - adds two numbers
+ * @n1: First number
+ * @n2: Second number
+ * @r: Buffer for results
+ * @size_r: Buffer size
+ * ops! Dreams not magic! Determination and hardwork
+ * Return: Address of r or 0
  */
-
-int main(void)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int n;
-	int a[5];
-	int *p;
+	int i, j, k, l, m, n;
 
-	a[2] = 1024;
-	p = &n;
-	*(p + 5) = 98;
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
