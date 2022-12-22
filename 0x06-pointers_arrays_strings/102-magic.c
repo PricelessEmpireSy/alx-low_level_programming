2c832886adcea497f6022fa5ed7742b3bb89bb2c
PricelessEmpireSy <misspriceless45@gmail.com>
2022-12-22 11:12:59 -0800

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,11 @@
+#include "main.h"
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: program prints a[2] = 98, followed by a new line.
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -8,16 +15,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
 	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
