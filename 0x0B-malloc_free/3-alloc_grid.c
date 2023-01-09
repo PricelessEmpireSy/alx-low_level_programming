ce1a39c4d92c590b2aa63745e164bcae14780e9a
PricelessEmpireSy <misspriceless45@gmail.com>
2023-01-09 08:29:44 -0800

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,6 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-
 	mee = malloc(sizeof(int *) * height);
 
 	if (mee == NULL)
@@ -31,7 +30,6 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-
 	for (x = 0; x < height; x++)
 	{
 		for (y = 0; y < width; y++)
